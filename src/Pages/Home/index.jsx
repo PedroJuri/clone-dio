33db350670057bdafad74cbc7a78d5baaa07a971
PedroJuri <p3dr1n018@outlook.com>
2023-06-22 22:47:27 -0300

    estilizando a pagina de login

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom"
 import { Button } from "../../Components/Button"
 import { Header } from "../../Components/Header"
 import bannerImage from '../../Assets/banner.png'
